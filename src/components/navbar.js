9fb2a8fa10ae1379faec716615cecd45700c503f
Jennifer Lam <jennifer@Jennifers-MacBook-Pro.local>
2025-04-09 15:58:38 -0700

    update navbar and add responsive mobile design for the mobile menu

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,10 +1,38 @@
-import React from 'react';
-import './navbar.css';
+import React from "react";
+import "./navbar.css";
 
-export default function Navbar(){
- return (
-  <div className="heading">
-   <h1>Welcome to Climate Nirvana</h1>
-  </div>
- );
+export default function Navbar() {
+    return (
+        <div className="heading">
+            <div class="nav-wrapper-desktop">
+                <div class="logo-wrapper">
+                    <img src="/images/logo.png" alt="earth-cartoon" />
+                    <h1>ecoTrekkers</h1>
+                </div>
+                <div class="nav-link-wrapper">
+                    <a href="">Home</a>
+                    <a href="">Glossary</a>
+                </div>
+                <div class="login-wrapper">
+                    <button>Login</button>
+                </div>
+            </div>
+            <div class="nav-wrapper-mobile">
+                <div class="logo-wrapper">
+                    <h1>ecoTrekkers</h1>
+                </div>
+                <div class="mobile-menu-toggle">
+                    <img src="/images/menu-icon.svg" alt="menu-icon" />
+                </div>
+                <div class="mobile-menu-wrapper active">
+                    <div class="mobile-menu-toggle">
+                        <img src="/images/close-icon.svg" alt="close-icon" />
+                    </div>
+                    <a href="">Home</a>
+                    <a href="">Glossary</a>
+                    <button>Login</button>
+                </div>
+            </div>
+        </div>
+    );
 }
