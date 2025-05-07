a48d85dc95060b2dafe04785d9bd9063d8e25ebb
gegsmithccsf <gsmith37@mail.ccsf.edu>
2025-05-06 19:44:03 -0700

    Added Fire Data Tiles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import * as React from "react";
 import { useState } from "react";
-import Map, { NavigationControl } from "react-map-gl";
+import Map, { NavigationControl, Source, Layer } from "react-map-gl";
 import Navbar from "./components/navbar.js";
 import maplibregl from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
 import "./App.css";
 import SearchBar from "./SearchBar";
+import * as pmtiles from "pmtiles";
 
 function App() {
     const [searchText, setSearchText] = useState("");
@@ -15,6 +16,9 @@ function App() {
     });
     const mapZoom = 5.3;
     const mapRef = React.useRef();
+
+    const protocol = new pmtiles.Protocol();
+    maplibregl.addProtocol('pmtiles', protocol.tile);
 
     return (
         <div className="App">
@@ -38,8 +42,30 @@ function App() {
 
                 onMove={(evt) => setMapCoords(evt.viewState)}
                 style={{ width: "100%", height: "calc(100vh - 80px)" }}
-                mapStyle="https://api.maptiler.com/maps/019684ce-bf99-76e5-a986-319c7bdeb2ab/style.json?key=2aydo59FoTATDd9gbc2J">
+                mapStyle="https://api.maptiler.com/maps/019684ce-bf99-76e5-a986-319c7bdeb2ab/style.json?key=2aydo59FoTATDd9gbc2J"
+            >
                 <NavigationControl position="top-left" />
+                <Source 
+                    id="fireData"
+                    type="vector"
+                    url="pmtiles://fire_risk.pmtiles"
+                >
+                    <Layer
+                        id="fireLayer"
+                        source="fireData"
+                        source-layer="California_High_Hazard_Zones_Tier_1"
+                        type="fill"
+                        paint={{"fill-color":"red", "fill-opacity":0.7}}
+                    />
+                    <Layer
+                        id="fireOutline"
+                        source="fireData"
+                        source-layer="California_High_Hazard_Zones_Tier_1"
+                        type="line"
+                        paint={{"line-color":"orange", "line-width":2}}
+                    />
+                </Source>
+                
             </Map>
         </div>
     );
